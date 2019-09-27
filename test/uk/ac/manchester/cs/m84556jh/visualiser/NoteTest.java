fea864d20c72b5d835889184741637ada1ac911d
Jack Harrison <jack.harrison-7@student.manchester.ac.uk>
2019-09-27 13:16:39 +0100

    Added colour tests to NoteTest

--- test/uk/ac/manchester/cs/m84556jh/visualiser/NoteTest.java
+++ test/uk/ac/manchester/cs/m84556jh/visualiser/NoteTest.java
@@ -2,12 +2,29 @@ package uk.ac.manchester.cs.m84556jh.visualiser;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import uk.ac.manchester.cs.m84556jh.colour.Col3;
+import uk.ac.manchester.cs.m84556jh.colour.ColPal;
 
 public class NoteTest {
 
 	private Note noteA4 = new Note(440.0, 20.43);
 	private Note noteG7 = new Note(3135.96, 30.35);
+	ColPal noteCols;
+	
+	@Before
+	public void setup() {
+		try {
+			noteCols = new ColPal(new File("colours.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void testGetOctave() {
@@ -31,6 +48,12 @@ public class NoteTest {
 	public void testGetAmp() {
 		assertTrue(((Double)noteA4.getAmp()).equals(20.43));
 		assertTrue(((Double)noteG7.getAmp()).equals(30.35));
+	}
+	
+	@Test
+	public void testGetCol() {
+		assertTrue(noteA4.getCol(noteCols).equals(new Col3(64,275,48)));
+		assertTrue(noteG7.getCol(noteCols).equals(new Col3(0,0,76)));
 	}
 	
 	@Test
