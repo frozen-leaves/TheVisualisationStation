cbaa17ac99e4f47fe975d0c0ebd5ee8d83e07dc8
Jack Harrison <jack.harrison-7@student.manchester.ac.uk>
2019-11-15 14:13:55 +0000

    Created new Visualisation class
    Split drawing of visualisation off into own class, to separate it from
    the Launcher, which was getting bloated. This class contains no
    calculation and simply takes the parameters from the Launcher and draws
    the appropriate visualisation based on these.

--- src/uk/ac/manchester/cs/m84556jh/visualiser/Launcher.java
+++ src/uk/ac/manchester/cs/m84556jh/visualiser/Launcher.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 
 import processing.core.PApplet;
 import processing.core.PImage;
-import uk.ac.manchester.cs.m84556jh.colour.Col;
 import uk.ac.manchester.cs.m84556jh.colour.ColPal;
 
 public class Launcher extends PApplet {
@@ -23,6 +22,7 @@ public class Launcher extends PApplet {
 	Key key;
 	BPM bpm;
 	String visType;
+	Visualisation vis = new Visualisation(this);
 	
 	public static void main(String[] args) {
 	    PApplet.main("uk.ac.manchester.cs.m84556jh.visualiser.Launcher");
@@ -53,12 +53,10 @@ public class Launcher extends PApplet {
     	key = new Key(p.keyBufSecs*fps);
     	bpm = new BPM(3*fps, p.bpmBufSize, fps);
 	    selectInput("Select a colour file to use:", "colsSelected");
-	    
     }
     
     public void mp3Selected(File mp3) {
     	spectrum = new Spectrum(this, mp3.getAbsolutePath(), 4096);
-    	
     }
     
     public void colsSelected(File cols) {
@@ -68,16 +66,6 @@ public class Launcher extends PApplet {
 			e.printStackTrace();
 		}
     	selectInput("Select an MP3 file to use:", "mp3Selected");
-    }
-    
-    //Display stats about the soundfile in real time
-    public void printStats(Note note, String noteKey, double curBPM) {
-		textSize(50);
-		text("Note:"+note.toString(), 10, 300);
-		text("Freq:"+note.getFreq(), 10, 350);
-		text("Max Oct:"+spectrum.getMaxOctave(), 10, 400);
-		text("Key:"+noteKey, 10, 450);
-		text("BPM:"+curBPM, 10, 500); 
     }
 	
     public void draw() {
@@ -89,50 +77,16 @@ public class Launcher extends PApplet {
 		
 		if(spectrum != null && noteCols != null) {
 			spectrum.analyse();
-			String noteKey = key.calc(spectrum.getMaxFreq().getIndex());
-			double curBPM = bpm.calcBPM(spectrum.getBeatAmp());
-			if(visType == "stats")
-				printStats(spectrum.getMaxFreq(), noteKey, curBPM);
-			Col noteCol = spectrum.getMaxFreq().getCol(noteCols);
-			Col[] ampCol = amp.getPixelBuf(noteCol, spectrum.getTotAmp());
 			
-			//If not printing stats (for main program), set the background colour to the key
-			Col keyCol = key.getCol(noteCols);
-			if(visType != "stats")
-				background(keyCol.getHue(), keyCol.getSat(), keyCol.getBri());
+			vis.draw(visType, 
+					 key.calc(spectrum.getMaxFreq().getIndex()), 
+					 key.getCol(noteCols), 
+					 bpm.calcBPM(spectrum.getBeatAmp()), 
+					 amp.getPixelBuf(spectrum.getMaxFreq().getCol(noteCols), spectrum.getTotAmp()), 
+					 bpm.isBeat(),
+					 spectrum.getMaxFreq(),
+					 spectrum.getMaxOctave());
 			
-			Col lastCol = null;
-			//Display shape of colours from colour array
-			for(int i = 0; i < ampCol.length; i++) {
-				//Set colour to the colour in the array element
-				fill(ampCol[i].getHue(), ampCol[i].getSat(), ampCol[i].getBri());
-				//Print the lines on the left and right of the display
-				rectMode(CENTER);
-				if(visType == "stats") {
-					rect((float)(width/2 + 1 - ampCol.length + i),(float)100.0,(float)1.0,(float)180.0);
-					rect((float)(width/2 + ampCol.length - i),(float)100.0,(float)1.0,(float)180.0);
-				}else if(visType == "rect"){
-					rect((float)(width/2 + 1 - ampCol.length + i),(float)(height/2),(float)1.0,(float)height);
-					rect((float)(width/2 + ampCol.length - i),(float)(height/2),(float)1.0,(float)height);
-				}else {
-					if(lastCol == null || !(lastCol.equals(ampCol[i]))) {
-						circle((float)(width/2), (float)(height/2), (float)(ampCol.length - i));
-						lastCol = ampCol[i];
-					}  
-				}
-			}
-			
-			//Display a red circle on each beat
-			if(bpm.isBeat()) {
-				if(visType == "stats") {
-					fill(0, 100, 50);
-					circle((float)(width/2), (float)(height-50), (float)50.0);
-				} else {
-					fill(0, 0, 100);
-					circle((float)(width/2), (float)(height-200), (float)200.0);
-				}
-				
-			}
 		}
 	}
 }
---
+++ src/uk/ac/manchester/cs/m84556jh/visualiser/Visualisation.java
@@ -0,0 +1,64 @@
+package uk.ac.manchester.cs.m84556jh.visualiser;
+
+import processing.core.PApplet;
+import processing.core.PConstants;
+import uk.ac.manchester.cs.m84556jh.colour.Col;
+
+public class Visualisation {
+	
+	private PApplet app;
+	
+	public Visualisation(PApplet app) {
+		this.app = app;
+	}
+	
+	//Display stats about the soundfile in real time
+    public void printStats(Note note, String noteKey, double curBPM, int maxOctave) {
+		app.textSize(50);
+		app.text("Note:"+note.toString(), 10, 300);
+		app.text("Freq:"+note.getFreq(), 10, 350);
+		app.text("Max Oct:"+maxOctave, 10, 400);
+		app.text("Key:"+noteKey, 10, 450);
+		app.text("BPM:"+curBPM, 10, 500); 
+    }
+	
+	public void draw(String type, String key, Col keyCol, double bpm, Col[] ampCol, boolean isBeat, Note maxNote, int maxOctave) {
+		//Print stats if that type, otherwise set background colour to the key colour
+		if(type == "stats")
+			printStats(maxNote, key, bpm, maxOctave);
+		else
+			app.background(keyCol.getHue(), keyCol.getSat(), keyCol.getBri());
+			
+		Col lastCol = null;
+		//Display shape of colours from colour array
+		for(int i = 0; i < ampCol.length; i++) {
+			//Set colour to the colour in the array element
+			app.fill(ampCol[i].getHue(), ampCol[i].getSat(), ampCol[i].getBri());
+			//Print the correct type of visualisation, based on type
+			app.rectMode(PConstants.CENTER);
+			if(type == "stats") {
+				app.rect((float)(app.width/2 + 1 - ampCol.length + i),(float)100.0,(float)1.0,(float)180.0);
+				app.rect((float)(app.width/2 + ampCol.length - i),(float)100.0,(float)1.0,(float)180.0);
+			}else if(type == "rect"){
+				app.rect((float)(app.width/2 + 1 - ampCol.length + i),(float)(app.height/2),(float)1.0,(float)app.height);
+				app.rect((float)(app.width/2 + ampCol.length - i),(float)(app.height/2),(float)1.0,(float)app.height);
+			}else {
+				if(lastCol == null || !(lastCol.equals(ampCol[i]))) {
+					app.circle((float)(app.width/2), (float)(app.height/2), (float)(ampCol.length - i));
+					lastCol = ampCol[i];
+				}  
+			}
+		}
+		
+		//Display a white circle on each beat
+		if(isBeat) {
+			if(type == "stats") {
+				app.fill(0, 100, 50);
+				app.circle((float)(app.width/2), (float)(app.height-50), (float)50.0);
+			} else {
+				app.fill(0, 0, 100);
+				app.circle((float)(app.width/2), (float)(app.height-200), (float)200.0);
+			}
+		}
+	}
+}
